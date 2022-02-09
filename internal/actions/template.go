ab18689e745d23103a063bc2bf01da5f0b1c33c5
dhuys <dhuys@vorys.com>
2022-02-09 18:53:36 +0000

    action file definitions and functions being worked on

--- internal/actions/template.go
+++ internal/actions/template.go
@@ -1,25 +1,97 @@
 package action
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/chigopher/pathlib"
+)
 
 type templateType string
+type JsonDecode interface {
+	Decode(data string)
+}
 
 const (
 	TriggerType       templateType = "trigger"
 	LinkedServiceType templateType = "linkedService"
 )
 
+var requiredActionTemplateKeys = []string{
+	"file", "type",
+}
+var validActionTemplateKeys = []string{
+	"file", "type", "add", "remove", "update",
+}
+
+// maps a template type to possible actions
 type ActionTemplate struct {
+	TargetFile   *pathlib.Path
 	TemplateType templateType
 	JsonActions  []JsonAction
 }
 
-// decode json object into template object
+// holds an entire action file that consists of
+// action templates
+type ActionFile struct {
+	ActionTemplates []ActionTemplate
+}
+
+// decodes json
 func (a *ActionTemplate) Decode(data string) {
 	var decode map[string]string
+	var actionTemplate ActionTemplate
 
 	if err := json.Unmarshal([]byte(data), &decode); err != nil {
 		log.Fatal(err)
 	}
 
+	// anonymous function needs to be here in order to
+	// calculatre length of `decode` after unmarshalling
+	contains := func(item string) bool {
+		set := make(map[string]struct{}, len(decode))
+		for s := range decode {
+			set[s] = struct{}{}
+		}
+		_, ok := set[item]
+		return ok
+	}
+
+	for _, requiredKey := range requiredActionTemplateKeys {
+		if !contains(requiredKey) {
+			log.Fatalf("Action template does not contain required key: %s", requiredKey)
+		}
+	}
+}
+
+// decode json object into action file structure
+func (a *ActionFile) Decode(data string) {
+	var decoded []map[string]interface{}
+
+	if err := json.Unmarshal([]byte(data), &decoded); err != nil {
+		log.Fatalf("Unable to parse file, %v\n", err)
+	}
+
+	var actionTemplates = make([]ActionTemplate, 0, len(decoded))
+
+	for _, template := range decoded {
+		templateStr, _ := json.Marshal(template)
+
+		actionTemplate := NewActionTemplate(string(templateStr))
+		actionTemplates = append(actionTemplates, actionTemplate)
+	}
+
+	a.ActionTemplates = actionTemplates
+}
+
+func NewActionTemplate(data string) ActionTemplate {
+	actionTemplate := ActionTemplate{}
+	actionTemplate.Decode(data)
+	return actionTemplate
+}
+
+func NewActionFile(data string) ActionFile {
+	actionFile := ActionFile{}
+	actionFile.Decode(data)
+
+	return actionFile
 }
--- internal/actions/template_test.go
+++ internal/actions/template_test.go
@@ -1,1 +1,73 @@
 package action
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/chigopher/pathlib"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/spf13/afero"
+)
+
+func TestDecodeActionTemplate(t *testing.T) {
+
+	template := `[
+			{
+				"file": "exampleTrigger.json",
+				"type": "trigger",
+				"add":[
+					{"path":"$.add.path", "value": "add1"},
+					{"path":"$.add.path2", "value": "add2"}
+				],
+				"remove": [
+					{"path": "$.remove.path", "value":"remove1"},
+					{"path": "$.remove.path2","value": "remove2"}
+				],
+				"update": [
+					{"path": "$.update.path", "value":"update1"},
+					{"path": "$.update.path2","value": "update2"}
+				]
+				
+			},
+			{
+				"file": "exampleLinkedService.json",
+				"type": "linkedService",
+				"add":[
+					{"path":"$.add.path","value": "add1"},
+					{"path":"$.add.path2","value": "add2"}
+				],
+				"remove": [
+					{"path": "$.remove.path","value": "remove1"},
+					{"path": "$.remove.path2","value": "remove2"}
+				],
+				"update": [
+					{"path": "$.update.path","value": "update1"},
+					{"path": "$.update.path2","value": "update2"}
+				]
+			}
+		]`
+
+	t.Run("Decode Trigger Template", func(t *testing.T) {
+
+		want := ActionTemplate{
+			TargetFile:   pathlib.NewPathAfero("exampleTrigger.json", afero.NewOsFs()),
+			TemplateType: TriggerType,
+			JsonActions: []JsonAction{
+				AddAction{jsonAction{Path: "$.add.path", Value: "add1"}},
+				AddAction{jsonAction{Path: "$.add.path2", Value: "add2"}},
+				RemoveAction{jsonAction{Path: "$.remove.path2", Value: "remove1"}},
+				RemoveAction{jsonAction{Path: "$.remove.path2", Value: "remove2"}},
+				UpdateAction{jsonAction{Path: "$.update.path2", Value: "update2"}},
+				UpdateAction{jsonAction{Path: "$.update.path2", Value: "update2"}},
+			},
+		}
+
+		got := NewActionFile(template)
+
+		if !reflect.DeepEqual(got, want) {
+			errMsg := fmt.Sprintf("got %+v\n\nwant%+v", spew.Sdump(got), spew.Sdump(want))
+			t.Error(errMsg)
+		}
+	})
+}
