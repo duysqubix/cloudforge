cd22bc82322a547765c00aa743a1851ee35ab0da
dhuys <dhuys@vorys.com>
2022-04-12 01:06:37 +0000

    Abstracted Synapse JSON files into pyObjects
    - Added ability to detect dependencies per resource

--- scripts/synapse/src/__init__.py
+++ scripts/synapse/src/__init__.py
@@ -1,4 +1,4 @@
-class _AzureResource:
+class AzResource:
     """
     Base object class that represents an azure resource
     """
@@ -6,3 +6,25 @@ class _AzureResource:
     def __init__(self, name: str, properties: dict):
         self.name = name
         self.properties = properties
+
+
+class AzDependency:
+
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+    def formatARM(self):
+        t = (self.type[0].lower() + self.type[1:]).replace("Reference",
+                                                           "") + "s"
+        return f"/{t}/{self.name}"
+
+    def __eq__(self, other):
+        return (self.name == other.name) and \
+               (self.type == other.type)
+
+    def __neq__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return self.formatARM()
